0a8f56afb9abb4ff92fe3f5b5f31bde774bc02ac
Moshe Atlow <moshea@testim.io>
2021-07-06 01:39:24 +0300

    benchmark

---
+++ tests/benchmarks/benchmarker.ts
@@ -0,0 +1,97 @@
+import * as mathjs from 'mathjs';
+
+type step = { step: string };
+type Func<T = any, U extends step = any> = (arg: T) => Promise<U>;
+
+export class BenchmarkerFlow<
+    A = any,
+    B extends step = any,
+    C extends step = any,
+    D extends step = any,
+    E extends step = any,
+    F extends step = any,
+    G extends step = any,
+> {
+    private flow: Func[];
+
+    constructor(
+        private options: { iterations: number; name: string },
+        a: Func<never, B>,
+        b?: Func<B, C>,
+        c?: Func<C, D>,
+        d?: Func<D, E>,
+        e?: Func<E, F>,
+        f?: Func<F, G>,
+    ) {
+        this.flow = [a, b, c, d, e, f].filter(Boolean) as Func[];
+    }
+
+    async *[Symbol.asyncIterator]() {
+        for (let i = 0; i < this.options.iterations; i++) {
+            let prevResult: any;
+            for (const item of this.flow) {
+                const startTime = process.hrtime.bigint();
+                prevResult = await item(prevResult);
+                const endTime = process.hrtime.bigint();
+                yield {
+                    name: this.options.name,
+                    step: prevResult.step,
+                    duration: Number(endTime - startTime) / 1000000,
+                };
+            }
+        }
+    }
+}
+
+async function* aggregateRoundRobin(sources: AsyncIterator<any>[]) {
+    do {
+        const results = await Promise.all(sources.map((s) => s.next()));
+        for (const i in results) {
+            const r = results[i];
+            if (r.done) {
+                sources.splice(Number(i), 1);
+            } else {
+                yield r.value;
+            }
+        }
+    } while (sources.length > 0);
+}
+
+export class Benchmarker {
+    private flows: BenchmarkerFlow[];
+    private durations: Record<string, Record<string, number[]>> = {};
+
+    constructor(...flows: BenchmarkerFlow[]) {
+        this.flows = flows;
+    }
+
+    async run() {
+        for await (const flow of aggregateRoundRobin(this.flows.map((f) => f[Symbol.asyncIterator]()))) {
+            this.durations[flow.name] = this.durations[flow.name] || {};
+            this.durations[flow.name][flow.step] = this.durations[flow.name][flow.step] || [];
+            this.durations[flow.name][flow.step].push(flow.duration);
+            console.log(flow.name, flow.step);
+        }
+    }
+
+    static stats(numbers: number[]) {
+        return {
+            mean: mathjs.mean(numbers),
+            stdev: mathjs.std(numbers),
+            min: mathjs.min(numbers),
+            max: mathjs.max(numbers),
+            percentaile95: mathjs.quantileSeq(numbers, 0.95),
+        };
+    }
+
+    get results() {
+        return Object.keys(this.durations).reduce((acc, key) => {
+            const durations = this.durations[key];
+            return Object.assign(acc, {
+                [key]: Object.keys(durations).reduce((acc, key) => {
+                    return Object.assign(acc, { [key]: Benchmarker.stats(durations[key]) });
+                }, {}),
+            });
+        }, {});
+    }
+}
---
+++ tests/benchmarks/index.ts
@@ -0,0 +1,153 @@
+import fetch from 'node-fetch';
+import * as fs from 'fs';
+import * as childProcess from 'child_process';
+import * as chromedriver from 'chromedriver';
+import { StandaloneServer } from '../../src/standalone/server';
+import { HubServer } from '../../src/grid/server';
+import { NodeServer } from '../../src/node/server';
+import { nodeConfig } from '../../src/node/config';
+import { copperConfig } from '../../src/standalone/config';
+import { Benchmarker, BenchmarkerFlow } from './benchmarker';
+import { delay } from '../../src/common/utils';
+
+const ITERATIONS = 200;
+
+const createSession = async <T extends { url: string }>(data: T) => {
+    const res = await fetch(`${data.url}/session`, {
+        method: 'POST',
+        body: JSON.stringify({
+            desiredCapabilities: {
+                browserName: 'chrome',
+                chromeOptions: { args: ['--headless', '--disable-gpu'] },
+            },
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+    const { sessionId, value } = await res.json();
+    return { ...data, sessionId: sessionId || value.sessionId, step: 'create session' };
+};
+const removeSession = async <T extends { url: string; sessionId: string }>(data: T) => {
+    await fetch(`${data.url}/session/${data.sessionId}`, { method: 'DELETE' });
+    return { ...data, step: 'remove session' };
+};
+const navigate = async <T extends { url: string; sessionId: string }>(data: T) => {
+    await fetch(`${data.url}/session/${data.sessionId}/url`, {
+        method: 'POST',
+        body: JSON.stringify({ url: 'https://www.google.com', waitUntil: 'load' }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+    return { ...data, step: 'navigate' };
+};
+
+async function measureBasicSession() {
+    const chromeDriver = new BenchmarkerFlow(
+        { iterations: ITERATIONS, name: 'ChromeDriver' },
+        async () => {
+            const driver = await chromedriver.start(['--url-base=/wd/hub', '--port=9516'], true);
+            return { driver, url: 'http://localhost:9516/wd/hub', step: 'start driver' };
+        },
+        createSession,
+        navigate,
+        removeSession,
+        async ({ driver }) => {
+            driver.kill();
+            return { step: 'kill driver' };
+        },
+    );
+
+    const copperStandalone = new BenchmarkerFlow(
+        { iterations: ITERATIONS, name: 'Copper Standalone' },
+        async () => {
+            const server = new StandaloneServer({ port: 4444, logLevel: 'silent' });
+            await server.listen();
+            return { server, url: 'http://localhost:4444/wd/hub', step: 'start driver' };
+        },
+        createSession,
+        navigate,
+        removeSession,
+        async ({ server }) => {
+            server.stop();
+            return { step: 'kill driver' };
+        },
+    );
+
+    const copperGrid = new BenchmarkerFlow(
+        { iterations: ITERATIONS, name: 'Copper Grid' },
+        async () => {
+            copperConfig.value.enableW3CProtocol = true;
+            const copperHub = new HubServer({ port: 4442, logLevel: 'silent' });
+            nodeConfig.value = { hubPort: 4442, port: 4443 };
+            const copperNode = new NodeServer({ port: 4443, logLevel: 'silent' });
+            await copperHub.listen();
+            await copperNode.listen();
+            return { copperHub, copperNode, url: 'http://localhost:4442/wd/hub', step: 'start driver' };
+        },
+        createSession,
+        navigate,
+        removeSession,
+        async ({ copperHub, copperNode }) => {
+            nodeConfig.reset();
+            await copperNode.stop();
+            await copperHub.stop();
+            return { step: 'kill driver' };
+        },
+    );
+
+    const seleniumStandalone = new BenchmarkerFlow(
+        { iterations: ITERATIONS, name: 'Selenium Standalone' },
+        async () => {
+            const server = childProcess.exec(
+                `java  -D${chromedriver.path} -jar ./selenium-server-standalone-3.141.59.jar -port 4441`,
+                { cwd: __dirname },
+            );
+            await delay(500);
+            return { server, url: 'http://localhost:4441/wd/hub', step: 'start driver' };
+        },
+        createSession,
+        navigate,
+        removeSession,
+        async ({ server }) => {
+            server.kill();
+            return { step: 'kill driver' };
+        },
+    );
+    const seleniumGrid = new BenchmarkerFlow(
+        { iterations: ITERATIONS, name: 'Selenium Grid' },
+        async () => {
+            const hubServer = childProcess.exec(
+                `java -jar ./selenium-server-standalone-3.141.59.jar -port 4439 -role hub`,
+                { cwd: __dirname },
+            );
+            await delay(500);
+            const nodeServer = childProcess.exec(
+                `java -jar -D${chromedriver.path} ./selenium-server-standalone-3.141.59.jar -port 4440 -role node -hubPort 4439`,
+                { cwd: __dirname },
+            );
+            await delay(1500);
+            return { nodeServer, hubServer, url: 'http://localhost:4439/wd/hub', step: 'start driver' };
+        },
+        createSession,
+        navigate,
+        removeSession,
+        async ({ nodeServer, hubServer }) => {
+            nodeServer.kill();
+            hubServer.kill();
+            return { step: 'kill driver' };
+        },
+    );
+
+    const marker = new Benchmarker(chromeDriver, copperStandalone, copperGrid, seleniumStandalone, seleniumGrid);
+    await marker.run();
+
+    console.log(marker.results);
+    fs.writeFileSync(`${__dirname}/results.json`, JSON.stringify(marker.results, null, 2));
+}
+
+(async function main() {
+    await measureBasicSession();
+})()
+    .then(() => process.exit(0))
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
